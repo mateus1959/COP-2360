5c5cbce349f4219b9923884a35da6acdc5c58bb8
St. Augustine <mateus1959@users.noreply.github.com>
2024-08-28 17:37:33 -0400

    Ran tests

--- PersonalInfo/PersonalInfo.cs
+++ PersonalInfo/PersonalInfo.cs
@@ -3,19 +3,19 @@ string name = "Lucinda Potter";
 string birthDate = "6/24/1992";
 string workPhone = "work 000-000-0101";
 string cellPhone = "cell 000-000-0189";
-Display(name, birthDate, workPhone, cellPhone);
+// Display(name, birthDate, workPhone, cellPhone);
 
 name = "John Smith";
 birthDate = "8/15/1978";
 workPhone = "work 000-111-0101";
 cellPhone = "cell 000-111-0189";
-Display(name, birthDate, workPhone, cellPhone);
+// Display(name, birthDate, workPhone, cellPhone);
 
 name = "Chris Johnson";
 birthDate = "12/01/1987";
 workPhone = "work 000-222-0101";
 cellPhone = "cell 000-222-0189";
-Display(name, birthDate, workPhone, cellPhone);
+// Display(name, birthDate, workPhone, cellPhone);
 
 name = "Robert Hall";
 birthDate = "2/27/1949";
@@ -29,3 +29,4 @@ static void Display(string name, string birthDate, string workPhone, string cell
     Console.WriteLine(workPhone);
     Console.WriteLine(cellPhone);
 }
+
