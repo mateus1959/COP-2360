6bf19c6d92b7767c57582bd76ea2623961646686
St. Augustine <mateus1959@users.noreply.github.com>
2024-08-28 17:24:14 -0400

    Wrote code for PersonalInfo

--- PersonalInfo/PersonalInfo.cs
+++ PersonalInfo/PersonalInfo.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = "Lucinda Potter";  
+string birthDate = "6/24/1992";
+string workPhone = "work 000-000-0101";
+string cellPhone = "cell 000-000-0189";
+Display(name, birthDate, workPhone, cellPhone);
+
+name = "John Smith";
+birthDate = "8/15/1978";
+workPhone = "work 000-111-0101";
+cellPhone = "cell 000-111-0189";
+Display(name, birthDate, workPhone, cellPhone);
+
+name = "Chris Johnson";
+birthDate = "12/01/1987";
+workPhone = "work 000-222-0101";
+cellPhone = "cell 000-222-0189";
+Display(name, birthDate, workPhone, cellPhone);
+
+name = "Robert Hall";
+birthDate = "2/27/1949";
+workPhone = "work 000-444-0101";
+cellPhone = "cell 000-444-0189";
+Display(name, birthDate, workPhone, cellPhone);
+
+public static void Display(string name, string birthDate, string workPhone, string cellPhone) {
+    Console.WriteLine(name);
+    Console.WriteLine(birthDate);
+    Console.WriteLine(workPhone);
+    Console.WriteLine(cellPhone);
+}
