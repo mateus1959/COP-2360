718d8754f90b8f583f8f0a9045e3e4b48bf8f043
St. Augustine <mateus1959@users.noreply.github.com>
2024-09-04 18:59:34 -0400

    Added Lyrics

---
+++ Lyrics/Lyrics/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Unit test 1
+// Console.WriteLine("Oh, well imagine /");
+// Console.WriteLine("As I'm pacing the pews in a church corridor / ");
+// Console.WriteLine("And I can't help but to hear /");
+// Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+
+// Console.WriteLine("Oh-oh-oh-oh-oh,");
+// Console.WriteLine("oh-oh-oh-oh,");
+// Console.WriteLine("oh-oh-oh /");
+// Console.WriteLine("Caught in a bad romance...");
+
+// Console.WriteLine("Coming out of my cage");
+// Console.WriteLine("And I've been doing just fine");
+// Console.WriteLine("Gotta gotta be down");
+// Console.WriteLine("Because I want it all");
+
+Console.WriteLine("Now, this is a story all about how");
+Console.WriteLine("My life got flipped-turned upside down");
+Console.WriteLine("And I'd like to take a minute");
+Console.WriteLine("Just sit right there");
+Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
+
+
