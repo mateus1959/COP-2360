e83cfa1f61a554ee49913f12cd0d23ce8162a1e2
St. Augustine <mateus1959@users.noreply.github.com>
2024-09-11 20:09:18 -0400

    Coded Program.cs

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+const double CentimetersPerInch = 2.54;
+double inches = 15994;
+double centimeters = inches * CentimetersPerInch;
+Console.WriteLine($"{inches} inches is {centimeters} centimeters.");
+
+
+
