f1f4102c96fa31acbc9fb769dcb79ab629020eb8
St. Augustine <mateus1959@users.noreply.github.com>
2024-09-12 23:26:56 -0400

    Converted program to accept user input for inches

--- InchesToCentimeters/Program.cs
+++ InchesToCentimeters/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 const double CentimetersPerInch = 2.54;
-double inches = 15994;
+Console.Write("Enter the number of inches: ");
+double inches = Convert.ToDouble(Console.ReadLine());
 double centimeters = inches * CentimetersPerInch;
 Console.WriteLine($"{inches} inches is {centimeters} centimeters.");
+
+
 
 
 
