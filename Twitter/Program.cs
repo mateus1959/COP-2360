32e9ec6eee85f3bd6612a00089e3fb3ef1a845e5
St. Augustine <mateus1959@users.noreply.github.com>
2024-09-18 12:28:25 -0400

    Code for twitter

---
+++ Twitter/Program.cs
@@ -0,0 +1,14 @@
+﻿// Accept a user’s message
+Console.Write("Enter your message: ");
+string msg = Console.ReadLine();
+const int maxLength = 140;
+
+// Check if the message length exceeds the maximum allowed length
+if (msg.Length > maxLength)
+{
+    Console.WriteLine("Rejected");
+}
+else
+{
+    Console.WriteLine("Posted");
+}
